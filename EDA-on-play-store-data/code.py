21b6106634ca582aaffb2a060552f81107b6d16b
Abhineetk <Abhineetk@users.noreply.github.com>
2020-07-21 11:07:45 +0530

    Code file created.

---
+++ EDA-on-play-store-data/code.py
@@ -0,0 +1,31 @@
+# --------------
+#Importing header files
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+
+
+
+
+#Loading the data
+data=pd.read_csv(path)
+
+#Code starts here
+#data.hist('Rating',bins=15, figsize=(8,7))
+data = data[data['Rating']<=5]
+#data.hist('Rating',bins=15, figsize=(8,7), color='red')
+data.isnull().sum()
+data = data.dropna()
+data.isnull().sum()
+
+
+
+
+
+
+
+
+
+
+
